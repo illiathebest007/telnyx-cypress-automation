f7a49f95dca808d82c9a5c330ac95be65ffdf10e
illia kaliiev <illiakaliiev@Noutbuk-illia.local>
2026-04-29 16:01:24 +0300

    add pom basepage

--- cypress/e2e/smoke_test.cy.js
+++ cypress/e2e/smoke_test.cy.js
@@ -1,34 +1,31 @@
 /// <reference types="cypress" />
+import BasePage from '../support/page_objects/BasePage'
 describe('telnyx smoke test cases', () => {
+  beforeEach(() => {
+    BasePage.visit(); // Подготовка: зашли
+    BasePage.acceptCookies(); // Подготовка: убрали баннер
+  });
   it('page loading', () => {
-    cy.visit('/')
     cy.title().should('include', 'Telnyx')
     cy.contains('Products').should('be.visible')
     cy.contains('Solutions').should('be.visible')
     cy.get('footer').scrollIntoView().should('be.visible')
   })
   it('clicking pricing -> voice AI', () => {
-    cy.visit('/')
-    cy.get('#onetrust-accept-btn-handler').click()
     cy.contains('Pricing').click({ force: true })
     cy.contains('VOICE AI', { matchCase: false }).click({ force: true })
     cy.contains('Voice AI Agents').should('be.visible')
   })
   it('full stack animation check', { scrollBehavior: 'top' }, () => {
-    cy.visit('/')
     cy.contains('EXPLORE THE STACK', { matchCase: false }).click({force:true})
     cy.contains('THE FULL STACK', { timeout: 25000}).should('exist')
   })
   it('on "full stack" animation click "configure agent"', () => {
-    cy.visit('/')
-    cy.get('#onetrust-accept-btn-handler').click()
     cy.contains('EXPLORE THE STACK', { matchCase: false }).click({force:true})
     cy.contains('CONFIGURE AGENT', { timeout: 15000, matchCase: false }).click()
     cy.contains('Configure the environment your agents run in', { matchCase: false }).should('be.visible')
   })
   it('on AI agent choose model "Kimi K2.5", send message "Hello"', () => {
-    cy.visit('/')
-    cy.get('#onetrust-accept-btn-handler').click()
     cy.contains('EXPLORE THE STACK', { matchCase: false }).click()
     cy.contains('CONFIGURE AGENT', { timeout: 15000, matchCase: false }).click()
     cy.contains('Kimi K2.5').click()
@@ -37,15 +34,11 @@ describe('telnyx smoke test cases', () => {
     cy.get('div.bg-transparent p', { timeout: 15000 }).should('not.be.empty').and('be.visible')
   })
   it('Scroll down to the footer menu and click "Terms and Conditions of Service"', () => {
-    cy.visit('/')
-    cy.get('#onetrust-accept-btn-handler').click()
     cy.get('footer').scrollIntoView()
     cy.contains('Terms and Conditions of Service', { matchCase: false }).click()
     cy.url().should('eq', Cypress.config().baseUrl + '/terms-and-conditions-of-service')
   })
   it('clicking contact us', () => {
-    cy.visit('/')
-    cy.get('#onetrust-accept-btn-handler').click()
     cy.contains('Contact us', { matchCase: false }).click({ force: true })
     cy.contains('Talk to an expert').should('be.visible')
   })
@@ -71,8 +64,6 @@ describe('telnyx smoke test cases', () => {
     cy.get('.mktoErrorMsg').should('be.visible').and('contain', 'This field is required.')
   })
   it('LinkedIn link', () => {
-    cy.visit('/')
-    cy.get('#onetrust-accept-btn-handler').click()
     cy.get('footer').scrollIntoView()
     cy.get('footer a[href*="linkedin"]').should('have.attr', 'href', 'https://www.linkedin.com/company/telnyx')
   })
---
+++ cypress/support/page_objects/BasePage.js
@@ -0,0 +1,9 @@
+class BasePage {
+    visit(url = '/') {
+        cy.visit(url)
+    }
+    acceptCookies() {
+        cy.get('#onetrust-accept-btn-handler').click()
+    }   
+}
+export default new BasePage() 
